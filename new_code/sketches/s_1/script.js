3e38138ffba0db52e54a6298a6bc87074ab3905c
jonasdrewling <jd311089@gmail.com>
2018-10-20 15:31:02 +0200

    got something working

--- new_code/sketches/s_1/script.js
+++ new_code/sketches/s_1/script.js
@@ -5,88 +5,57 @@ function onDocumentReady() {
     el.addEventListener('pointerdown', onPointerDown);
     el.addEventListener('pointermove', onPointerMove);
     el.addEventListener('pointerleave', onPointerLeave);
-
-    // prevent default gestures in iOS
-    el.addEventListener('gesturestart', e => e.preventDefault());
-    el.addEventListener('gesturechange', e => e.preventDefault());
-    el.addEventListener('gestureend', e => e.preventDefault());
-
-    el = document.getElementById("data");
-    // prevent default gestures in iOS
-    el.addEventListener('gesturestart', e => e.preventDefault());
-    el.addEventListener('gesturechange', e => e.preventDefault());
-    el.addEventListener('gestureend', e => e.preventDefault());
-  
-
-    setInterval(() => {
-      displayData();
-    }, 100)
-
   }
   
-  function displayData () {
-    let g = pointers.createPointerGroupings();
-    let text = "";
-    if (pointers.numOfPointers > 1) 
-      var { distance, isApproaching } = pointers.comparePointers(pointers.pointerIds[0], pointers.pointerIds[1]);      
-    
-    if (g.numOfGroupings > 0) {
-      let pg = g.groupings[0];
+  let distanceOnPointerDown = 2;
 
-
-      text ="Moving up: " + pg.isMovingUp + "<br/>" +
-            "Moving left: " + pg.isMovingLeft + "<br/>" +  
-            "Number of groupings: " + g.numOfGroupings + "<br/>" +
-            "Horizontal speed: " + Math.round(pg.horizontalSpeed)+"<br/>"   +
-            "Vertical speed: " + Math.round(pg.verticalSpeed)+"<br/>"  +
-            "Absolut speed: " + Math.round(pg.speed)+"<br/>" +
-            "Num of pointers: " + pointers.numOfPointers + "<br/>"+
-            "Distance between 1. & 2. pointers: " + distance + "<br/>" +
-            "Pointer 1 & 2 approaching: " + isApproaching;
-
-    } 
-    document.getElementById("data").innerHTML = text;
-  }
+  // function setSplitPercent (percent) {
+  //   // snap if percent is less than 10
+  //   let split = document.getElementById("split");
+  //   if (percent < 10) {
+  //     split.style.borderStyle = "hidden";
+  //     percent = 0;
+  //   }
+  //   else
+  //     split.style.borderStyle = "solid";
+  //   split.style.height = `${percent}%`;
+  //   document.getElementById("layer0").style.height = `${(100-percent)/2}%`;
+  //   document.getElementById("layer1").style.height = `${(100-percent)/2}%`;
+  // }
 
   function onPointerUp(e) {
-    pointers.removePointer(e.pointerId);
-    let el = getOrCreate(e);
-    el.classList.remove('down');
+    pointers.removePointer(e);
+    // let warm1 = getOrCreate(e);
+    // document.body.removeChild(warm1);
   }
   
   function onPointerDown(e) {
     e.preventDefault();
     pointers.updatePointer(e);
+    if (pointers.numOfPointers == 2) {
+      let id1 = pointers.pointerIds[0], id2 = pointers.pointerIds[1];
+      let { distance } = pointers.comparePointers(id1, id2);
+      distanceOnPointerDown = distance;
+    }
   }
   
   function onPointerLeave(e) {
-    pointers.removePointer(e.pointerId);
-    e.preventDefault();
-    let el = getOrCreate(e);
-    document.body.removeChild(el);
+    pointers.removePointer(e);
   }
   
-  function onPointerMove(e) {
-     pointers.updatePointer(e); 
-     let b = document.body; 
-     let el = getOrCreate(e);
-     let hs = pointers.getPointer(e.pointerId).horizontalSpeed;
-     let twoPoint = pointers.numOfPointers;
-  
-     e.preventDefault();
-  
-    // Position the element from its middle
-    let rect = el.getBoundingClientRect();
-    el.style.left = (e.clientX-rect.width/2) + 'px';
-    el.style.top = (e.clientY-rect.height/2) + 'px';
+  // ----------OPENING THE WARM SPACE----------
 
-  //  if (twoPoint == 2) {
-  //    el.style.backgroundColor = 'green';
-  //    b.style.backgroundColor = 'purple';
-  //  } 
+  function onPointerMove(e) {
+    pointers.updatePointer(e);  //UPDATE POINTERTS FOR EVERY FUNCTION
+    let warm1 = getOrCreate(e);
+
+    e.preventDefault();
+
+    let rect = warm1.getBoundingClientRect();
+    warm1.style.left = (e.clientX-rect.width/2) + 'px';
+    warm1.style.top = (e.clientY-rect.height/2) + 'px'; 
   }
-  
-  // Returns an existing element for a pointer id, or makes a new one
+
   function getOrCreate(evt) {
     const id = 'pointer-' + evt.pointerId;
     let el = document.getElementById(id);
@@ -97,13 +66,12 @@ function onDocumentReady() {
     el.addEventListener('gesturestart', e => e.preventDefault());
     el.addEventListener('gesturechange', e => e.preventDefault());
     el.addEventListener('gestureend', e => e.preventDefault());
-  
 
     el.id = id;
     document.body.appendChild(el);
     return el;
   }
-  
+
   if (document.readyState != 'loading') onDocumentReady();
   else document.addEventListener('DOMContentLoaded', onDocumentReady);
   
